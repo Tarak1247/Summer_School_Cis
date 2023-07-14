ee8265463de05f05f87dc768443d3f23725fb26f
Jayanth <jayanth.sms.in@gmail.com>
2023-07-14 22:29:30 +0530

    fixed Bugs & final Version Submitted by Jay

---
+++ src/component/Sponsors/sponsersData.js
@@ -0,0 +1,42 @@
+
+import Ieee from  "./sponsorlogo/ieee.png";
+import IeeeCis from  "./sponsorlogo/ieeecis.png";
+import IeeeCusb from  "./sponsorlogo/ieeecusb.png";
+import CU from "./sponsorlogo/chandigarhuniversity.png";
+
+const communitypartner = [{
+        img: Ieee,
+        txt: " IEEE",
+        link: "",
+    },
+    {
+        img: IeeeCis,
+        txt: " IEEE CIS",
+        link: " ",
+    },
+    {
+        img: IeeeCusb,
+        txt: " IEEE Cusb",
+        link: " ",
+    },
+];
+
+const mediapartner = [{
+    /* img: lorem,*/ 
+    txt: " ",
+    link: " ",
+}, ];
+
+const venuepartner = [{
+    img: CU,
+    txt:  " Chandigarh University",
+    link: " ",
+}, ];
+
+const sponsers = {
+   communitypartner,
+   venuepartner,
+   mediapartner
+};
+
+export default sponsers;

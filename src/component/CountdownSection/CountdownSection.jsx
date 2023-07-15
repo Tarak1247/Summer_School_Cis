150b3f8c1561a19d91ff23159eb986e30367ba99
Tarak Aggarwal <116876777+Tarak1247@users.noreply.github.com>
2023-07-15 16:51:44 +0530

    Update CountdownSection.jsx

--- src/component/CountdownSection/CountdownSection.jsx
+++ src/component/CountdownSection/CountdownSection.jsx
@@ -10,7 +10,7 @@ const CountdownSection = () => {
       <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { duration: 1 } }} viewport={{ once: false }}  className="event__section2" id="registration" >
         <div className="event_countdown_main">
           <div>
-            <h1>Registration Starting in </h1>
+            <h1>Registration Closing in </h1>
             <hr style={{ marginBottom: "70px" }} />
             {/* <h1>TBA</h1> */}
             <Timer />
